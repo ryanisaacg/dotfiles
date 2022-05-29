b1b361b4d2ced9c37bf7543ee8e86ee37e3e8558
Ryan Goldstein <ryan@ryanisaacg.com>
2022-05-29 10:08:53 -0400

    Add an option for ez reviews

--- fish/functions/review.fish
+++ fish/functions/review.fish
@@ -1,6 +1,9 @@
 function review
     set pr (gh pr status --json number,title,url | jq ".createdBy|map(select(.number==$argv[1]))[0]")
     set title (echo "$pr" | jq ".title" -r)
+    if [ "$argv[2]" = "ez" ]
+        set title "[ez review] $title"
+    end
     set url (echo "$pr" | jq ".url" -r)
     echo "Copied! $title $url"
     echo -n "$title $url" | pbcopy
