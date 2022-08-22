9c96d0526a07239acdadb5e0d9a3df4277862684
Ryan Goldstein <ryan@ryanisaacg.com>
2022-08-22 09:29:09 -0400

    Add a script to rebase all my branches to dev

---
+++ fish/functions/git-rebase-all.fish
@@ -0,0 +1,31 @@
+function git-rebase-all
+    echo "* Pulling latest from origin..."
+    git checkout dev
+    git pull
+    set branches (git for-each-ref --format='%(refname:short)' refs/heads)
+    echo "* Looping over your branches..."
+    set prs (gh pr status --json headRefName,number,title | jq '.createdBy')
+    for branch in $branches
+        if [ "$branch" = "dev" ]
+            continue
+        end
+        git checkout $branch
+        set branch_info (echo "$prs" | jq "map(select(.headRefName==\"$branch\"))[0]")
+        if [ "$branch_info" != "null" ]
+            echo "This branch is pushed to GitHub! Recommend merge instead of rebase"
+        end
+        read -l -P 'Rebase, merge, or skip this branch? quit? [R/m/s/q] ' action
+       switch $action
+          case R r ''
+            git rebase -i dev
+          case M m
+            git merge dev
+          case S s
+          case Q q
+            break
+        end
+        if test $status -ne 0
+            read -l -P "Likely merge conflict occurred; resolve and hit 'enter'" _
+        end
+    end
+end
