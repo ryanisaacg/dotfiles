9c84a0b715cb4b580955122b0ae1de40dda9bd34
Ryan Goldstein <ryan@ryanisaacg.com>
2023-03-08 14:56:48 -0500

    Add keybinds for telescope

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,3 +1,4 @@
+-- LSP-specific keybinds live in lua/lsp.lua
 local util = require("util")
 
 vim.g.mapleader = ' '
@@ -12,3 +13,9 @@ util.leadmap('-', ':split\n')
 util.leadmap('|', ':vsplit\n')
 
 util.leadmap('d', ":put =strftime('Time: %a %Y-%m-%d %H:%M:%S')<CR>") -- Insert timestamp
+
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<leader>tg', telescope.live_grep, {noremap=true, silent=true})
+vim.keymap.set('n', '<leader>tf', telescope.find_files, {noremap=true, silent=true})
+vim.keymap.set('n', '<leader>tb', telescope.buffers, {noremap=true, silent=true})
+vim.keymap.set('n', '<leader>tr', telescope.reloader, {noremap=true, silent=true})
