0f31925bc48b0c0e213bb58e00fcf88c5a4b85c4
Ryan Goldstein <ryan@ryanisaacg.com>
2023-04-10 09:39:19 -0400

    Rewrite attach script for use with many active tmux sessions on the same host

--- fish/functions/attach.fish
+++ fish/functions/attach.fish
@@ -1,4 +1,12 @@
-function attach
-    tmux attach -t 0
+function attach -a name
+    set base (basename $PWD)
+    if not set -q name
+        set name $base
+    end
+    set sessions (tmux list-sessions -F '#{session_name}')
+    if contains $name $sessions
+        tmux attach -t $name
+    else
+        tmux new -s $name
+    end
 end
-
