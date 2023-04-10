85aff057cbe6c59b7705fedf1db49febc276cd49
Ryan Goldstein <ryan@ryanisaacg.com>
2023-04-10 12:34:38 -0400

    Support non 'dev' remote branches

--- fish/functions/git-smartlog.fish
+++ fish/functions/git-smartlog.fish
@@ -3,13 +3,15 @@ function git_smartlog
     set remote_branches (git branch -r)
     set prs (gh pr status --json headRefName,number,title | jq '.createdBy')
     set current (git branch --show-current)
+    set default_remote_branch (git rev-parse --abbrev-ref origin/HEAD)
+
     for branch in $branches
         if [ "$branch" = "$current" ]
             echo -n "* "
         else
             echo -n "  "
         end
-        set commits_behind (git rev-list --left-only --count origin/dev...$branch)
+        set commits_behind (git rev-list --left-only --count $default_remote_branch...$branch)
         set commits_ahead (git rev-list --right-only --count origin/$branch...$branch 2>&1)
         # TODO: clean this up, current logic is a mess
         if [ $status = 0 ]; and [ "$commits_ahead" != "0" ];
@@ -32,6 +34,8 @@ function git_smartlog
             set title (echo "$branch_info" | jq ".title" -r)
             set cols (expr (tput cols) - 5)
             echo "$branch$commits - [$number] $title" | sed "s/\(.\{$cols\}\).*/\1.../"
+        else if [ "origin/$branch" = "$default_remote_branch" ]
+            echo "$branch$commits - Default"
         else if contains "  origin/$branch" $remote_branches
             echo "$branch$commits - Remote"
         else
