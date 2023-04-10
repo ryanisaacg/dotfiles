3f095f4870411949239bb008c36a1b69f4fe0305
Ryan Goldstein <ryan@ryanisaacg.com>
2023-04-10 09:39:47 -0400

    Tweak review script output

--- fish/functions/review.fish
+++ fish/functions/review.fish
@@ -2,7 +2,7 @@ function review
     set pr (gh pr status --json number,title,url | jq ".createdBy|map(select(.number==$argv[1]))[0]")
     set title (echo "$pr" | jq ".title" -r)
     if [ "$argv[2]" = "ez" ]
-        set title "[ez] $title"
+        set title "[smol] $title"
     end
     set url (echo "$pr" | jq ".url" -r)
     echo "Copied! $title $url"
