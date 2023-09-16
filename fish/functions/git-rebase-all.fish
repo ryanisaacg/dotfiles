3ee87f7a4c92791e9ff10d12d7977a19666ad339
Ryan Goldstein <ryan@ryanisaacg.com>
2023-09-16 09:08:35 -0400

    Change base branch for rebase-all

--- fish/functions/git-rebase-all.fish
+++ fish/functions/git-rebase-all.fish
@@ -1,12 +1,12 @@
 function git-rebase-all
     echo "* Pulling latest from origin..."
-    git checkout dev
+    git checkout main
     git pull
     set branches (git for-each-ref --format='%(refname:short)' refs/heads)
     echo "* Looping over your branches..."
     set prs (gh pr status --json headRefName,number,title | jq '.createdBy')
     for branch in $branches
-        if [ "$branch" = "dev" ]
+        if [ "$branch" = "main" ]
             continue
         end
         git checkout $branch
@@ -17,9 +17,9 @@ function git-rebase-all
         read -l -P 'Rebase, merge, or skip this branch? quit? [R/m/s/q] ' action
        switch $action
           case R r ''
-            git rebase -i dev
+            git rebase -i main
           case M m
-            git merge dev
+            git merge main
           case S s
           case Q q
             break
