9c2d52787d67d0ef788e1b4254ca22ec5443799c
Ryan <ryanisaacg@users.noreply.github.com>
2024-07-05 14:57:08 -0400

    wasmtime

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,8 @@ set -x FZF_DEFAULT_COMMAND "rg -l ."
 set -x EDITOR nvim
 
 alias g='git'
+
+set -gx WASMTIME_HOME "$HOME/.wasmtime"
+
+string match -r ".wasmtime" "$PATH" > /dev/null; or set -gx PATH "$WASMTIME_HOME/bin" $PATH
+
