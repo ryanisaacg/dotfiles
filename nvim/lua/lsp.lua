cf75bc40f726ae592cd08039778a1befeeae76ba
Ryan <ryanisaacg@users.noreply.github.com>
2024-07-05 15:04:44 -0400

    Revert C# config

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -3,7 +3,7 @@ local util = require("util")
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   -- Enable completion triggered by <c-x><c-o>
@@ -27,83 +27,6 @@ local on_attach = function(client, bufnr)
   util.keymap(']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
   util.keymap('<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>')
   util.keymap('<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-
-  if client.name == "omnisharp" then
-    client.server_capabilities.semanticTokensProvider = {
-      full = vim.empty_dict(),
-      legend = {
-        tokenModifiers = { "static_symbol" },
-        tokenTypes = {
-          "comment",
-          "excluded_code",
-          "identifier",
-          "keyword",
-          "keyword_control",
-          "number",
-          "operator",
-          "operator_overloaded",
-          "preprocessor_keyword",
-          "string",
-          "whitespace",
-          "text",
-          "static_symbol",
-          "preprocessor_text",
-          "punctuation",
-          "string_verbatim",
-          "string_escape_character",
-          "class_name",
-          "delegate_name",
-          "enum_name",
-          "interface_name",
-          "module_name",
-          "struct_name",
-          "type_parameter_name",
-          "field_name",
-          "enum_member_name",
-          "constant_name",
-          "local_name",
-          "parameter_name",
-          "method_name",
-          "extension_method_name",
-          "property_name",
-          "event_name",
-          "namespace_name",
-          "label_name",
-          "xml_doc_comment_attribute_name",
-          "xml_doc_comment_attribute_quotes",
-          "xml_doc_comment_attribute_value",
-          "xml_doc_comment_cdata_section",
-          "xml_doc_comment_comment",
-          "xml_doc_comment_delimiter",
-          "xml_doc_comment_entity_reference",
-          "xml_doc_comment_name",
-          "xml_doc_comment_processing_instruction",
-          "xml_doc_comment_text",
-          "xml_literal_attribute_name",
-          "xml_literal_attribute_quotes",
-          "xml_literal_attribute_value",
-          "xml_literal_cdata_section",
-          "xml_literal_comment",
-          "xml_literal_delimiter",
-          "xml_literal_embedded_expression",
-          "xml_literal_entity_reference",
-          "xml_literal_name",
-          "xml_literal_processing_instruction",
-          "xml_literal_text",
-          "regex_comment",
-          "regex_character_class",
-          "regex_anchor",
-          "regex_quantifier",
-          "regex_grouping",
-          "regex_alternation",
-          "regex_text",
-          "regex_self_escaped_character",
-          "regex_other_escape",
-        },
-      },
-      range = true,
-    }
-  end
 end
 
 local function filter(arr, fn)
@@ -121,7 +44,6 @@ local function filter(arr, fn)
   return filtered
 end
 
-local pid = vim.fn.getpid()
 local language_servers = {
     tsserver = {
       handlers = {
@@ -172,61 +94,7 @@ local language_servers = {
             },
         },
     },
-    terraformls = {},
-    omnisharp = {
-        cmd = { "mono", "/Users/ryanisaacg/bin/omnisharp-osx/omnisharp/OmniSharp.exe", "--languageserver", "--hostPID", tostring(pid) },
-        handlers = {
-            ["textDocument/definition"] = require('omnisharp_extended').handler,
-        },
-
-        -- Enables support for reading code style, naming convention and analyzer
-        -- settings from .editorconfig.
-        enable_editorconfig_support = true,
-
-        -- If true, MSBuild project system will only load projects for files that
-        -- were opened in the editor. This setting is useful for big C# codebases
-        -- and allows for faster initialization of code navigation features only
-        -- for projects that are relevant to code that is being edited. With this
-        -- setting enabled OmniSharp may load fewer projects and may thus display
-        -- incomplete reference lists for symbols.
-        enable_ms_build_load_projects_on_demand = false,
-
-        -- Enables support for roslyn analyzers, code fixes and rulesets.
-        enable_roslyn_analyzers = false,
-
-        -- Specifies whether 'using' directives should be grouped and sorted during
-        -- document formatting.
-        organize_imports_on_format = false,
-
-        -- Enables support for showing unimported types and unimported extension
-        -- methods in completion lists. When committed, the appropriate using
-        -- directive will be added at the top of the current file. This option can
-        -- have a negative impact on initial completion responsiveness,
-        -- particularly for the first few completion sessions after opening a
-        -- solution.
-        enable_import_completion = true,
-
-        -- Specifies whether to include preview versions of the .NET SDK when
-        -- determining which version to use for project loading.
-        sdk_include_prereleases = true,
-
-        -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-        -- true
-        analyze_open_documents_only = false,
-    },
-    zls = {},
-    brick_lsp = {},
 }
-
--- Indicate that we should have autocompletion - required for nvim-cmp
--- Also mark dynamciRegistration as true for Unity/C# stuff
-local capabilities = vim.tbl_deep_extend("force", require('cmp_nvim_lsp').default_capabilities(), {
-    workspace = {
-        didChangeWatchedFiles = {
-            dynamicRegistration = true,
-        },
-    },
-})
 
 -- Patch brick_lsp into the config
 require('lspconfig.configs').brick_lsp = {
@@ -240,6 +108,9 @@ require('lspconfig.configs').brick_lsp = {
     };
 }
 
+
+-- Indicate that we should have autocompletion - required for nvim-cmp
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 for server,settings in pairs(language_servers) do
     settings.on_attach = on_attach
